168b33a469948de62e3056f1d4459a044538aadc
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-25 12:52:10 -0800

    Capacity: add max-width to usage so bar can grow

--- server.ts
+++ server.ts
@@ -621,7 +621,7 @@ if (isProduction) {
 // DB version: stored in DB, auto-updates on data changes
 
 const SITE_VERSION = 'v260225'  // Manual update on code changes
-const SITE_TIME = '1250'
+const SITE_TIME = '1251'
 
 const VERSION_KEY = 'dcc_versions'
 
