18e3aec1e16e7d8f617837cce2b59f854a88c10e
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-25 09:28:14 -0800

    Match tooltip-trigger styling to action-btn

--- server.ts
+++ server.ts
@@ -475,7 +475,7 @@ if (isProduction) {
 // DB version: stored in DB, auto-updates on data changes
 
 const SITE_VERSION = 'v260225'  // Manual update on code changes
-const SITE_TIME = '0924'
+const SITE_TIME = '0927'
 
 const VERSION_KEY = 'dcc_versions'
 
