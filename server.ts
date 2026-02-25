55e853352a390ac34606ae17046f921b316a2a59
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-25 10:38:14 -0800

    Force redeploy

--- server.ts
+++ server.ts
@@ -523,7 +523,7 @@ if (isProduction) {
 // DB version: stored in DB, auto-updates on data changes
 
 const SITE_VERSION = 'v260225'  // Manual update on code changes
-const SITE_TIME = '1020'
+const SITE_TIME = '1033'
 
 const VERSION_KEY = 'dcc_versions'
 
