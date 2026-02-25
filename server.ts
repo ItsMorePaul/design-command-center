ca9d5cf9507bbea04287fc1560391481ec2f0100
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-25 12:42:49 -0800

    Capacity: min-width on name (6 chars), hide bar/hours on mobile

--- server.ts
+++ server.ts
@@ -621,7 +621,7 @@ if (isProduction) {
 // DB version: stored in DB, auto-updates on data changes
 
 const SITE_VERSION = 'v260225'  // Manual update on code changes
-const SITE_TIME = '1239'
+const SITE_TIME = '1242'
 
 const VERSION_KEY = 'dcc_versions'
 
