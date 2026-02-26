3d4d551aa5e316f787ae80a5245b68bc7910837d
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 07:36:43 -0800

    Bump version v260226|0736

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260225'  // Manual update on code changes
-const SITE_TIME = '2152'
+const SITE_VERSION = 'v260226'  // Manual update on code changes
+const SITE_TIME = '0736'
 
 const VERSION_KEY = 'dcc_versions'
 
