b809a6bba27d45529c9c2de1e442e5b6372ac24d
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 19:14:05 -0800

    Correct site timestamp to 1913

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260226|1920'  // Manual update on code changes
-const SITE_TIME = '1920'
+const SITE_VERSION = 'v260226|1913'  // Manual update on code changes
+const SITE_TIME = '1913'
 
 const VERSION_KEY = 'dcc_versions'
 
