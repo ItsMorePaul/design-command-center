c79fa983f872495dc5655831cc729de1770062a7
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 20:01:46 -0800

    Priority BL select: match sort-btn height, padding, font-size, border-radius

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260226|2008'  // Manual update on code changes
-const SITE_TIME = '2008'
+const SITE_VERSION = 'v260226|2012'  // Manual update on code changes
+const SITE_TIME = '2012'
 
 const VERSION_KEY = 'dcc_versions'
 
