578cf2ca8c8db741ba413fbfc86d62a0b731a4f5
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 19:14:51 -0800

    Save site: drag-and-drop timeline ranges, DB version format fix, docs updated

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260226|1913'  // Manual update on code changes
-const SITE_TIME = '1913'
+const SITE_VERSION = 'v260226|1914'  // Manual update on code changes
+const SITE_TIME = '1914'
 
 const VERSION_KEY = 'dcc_versions'
 
