daa5303b6d82d09f5f447e0eea93a115ae69ecfd
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 19:03:21 -0800

    Fix generateDbVersionParts: db_version now includes time (v260226|HHMM format)

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260226|1915'  // Manual update on code changes
-const SITE_TIME = '1915'
+const SITE_VERSION = 'v260226|1920'  // Manual update on code changes
+const SITE_TIME = '1920'
 
 const VERSION_KEY = 'dcc_versions'
 
@@ -648,7 +648,7 @@ const generateDbVersionParts = () => {
   const min = get('minute')
 
   return {
-    versionNumber: `v${yy}${mm}${dd}`,
+    versionNumber: `v${yy}${mm}${dd}|${hh}${min}`,
     versionTime: `${hh}${min}`,
   }
 }
