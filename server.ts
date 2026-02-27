2396889b2732dda5e43732f4e2941d0493f2b03c
Wilson <wilson@Wilsons-Mac-mini.local>
2026-02-26 18:19:07 -0800

    Custom links: floating label layout, paired columns, consistent remove button

--- server.ts
+++ server.ts
@@ -621,8 +621,8 @@ if (isProduction) {
 // Site version: manually updated in code when commits are made (vYYMMDD.hhmm)
 // DB version: stored in DB, auto-updates on data changes
 
-const SITE_VERSION = 'v260226|1835'  // Manual update on code changes
-const SITE_TIME = '1835'
+const SITE_VERSION = 'v260226|1840'  // Manual update on code changes
+const SITE_TIME = '1840'
 
 const VERSION_KEY = 'dcc_versions'
 
